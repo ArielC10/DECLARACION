429c0d7feb0bc9a72b9685df9ec63f3995107d18
ArielC10 <112737418+ArielC10@users.noreply.github.com>
2024-02-29 18:56:42 -0500

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Aqui tu le dices cualquier vaina, yo quite lo que tenia porque que verguenza ')
+    alert('Aqui tu le dices cualquier vaina, yo quite lo que tenia porque que verguenza')
 });
 
 const noBtn = document.querySelector('#noBtn');
